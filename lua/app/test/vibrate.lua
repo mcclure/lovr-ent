307e89d6ea7351f7d594520f91865149160d3b57
mcc <andi.m.mcclure@gmail.com>
2019-10-11 17:20:01 -0400

    Fix default app (oops) and lengths in vibrate test
    Now up to date with main repo andi branch hg:14de519d5845 git:18de7cffaf39

--- lua/app/test/vibrate.lua
+++ lua/app/test/vibrate.lua
@@ -14,11 +14,11 @@ function VibrateTest:onUpdate()
 	for i,controllerName in ipairs(lovr.headset.getHands()) do
 		local length
 		if lovr.headset.isDown(controllerName, 'y') or lovr.headset.isDown(controllerName, 'b') then
-			length = 0
+			length = 0.2
 		elseif lovr.headset.isDown(controllerName, 'x') or lovr.headset.isDown(controllerName, 'a') then
 			length = 0.04
 		elseif lovr.headset.isDown(controllerName, 'trigger') then
-			length = 0.5
+			length = 1
 		end
 		if not self.vibrations[i] then
 			if length then
@@ -30,7 +30,7 @@ function VibrateTest:onUpdate()
 end
 
 function VibrateTest:onDraw()
-	lovr.graphics.print('Y or B: 0ms\nX or A: 0.05ms\nTrigger: 0.5ms\nAll intensities: '..intensity, 0, 1.7, -3, .2)
+	lovr.graphics.print('Y or B: 0.2ms\nX or A: 0.05ms\nTrigger: 1s\nAll intensities: '..intensity, 0, 1.7, -3, .2)
 end
 
 return VibrateTest
--- lua/main.lua
+++ lua/main.lua
@@ -35,7 +35,7 @@ end)
 
 namespace "standard"
 
-local defaultApp = "app/test/vibrate"
+local defaultApp = "app/test/cube"
 
 function lovr.load()
 	ent.root = LoaderEnt(#arg > 0 and arg or {defaultApp})
